dacab3f1488b87edab1c5bc874827bea5c325b53
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2019-04-22 14:17:23 +0200

    Delete ArrayShuffelingFunction.js

--- ArrayShuffelingFunction.js
+++
@@ -1,7 +0,0 @@
-function shuffleArray(array) {
-  for(let oldPos = array.length - 1; oldPos > 0; oldPos--) {
-    const newPos = Math.floor(Math.random() * (oldPos+1));
-    [array[oldPos], array[newPos]] = [array[newPos],array[oldPos]];
-  }
-  return array;
-}
